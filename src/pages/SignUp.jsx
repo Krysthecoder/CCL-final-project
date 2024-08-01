6f53c950817751b1cda633065b78071a91cd9566
The Krys <krysthopher@gmail.com>
2024-07-31 23:53:18 -0600

    signup page created

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -6,9 +6,12 @@ import { utilsData } from '../utils/utilsData';
 export default function SignUp() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [singupStatus, setSignupStatus] = useState('initialStatus');
+  const [userMessage, setUserMessage] = useState('');
 
   async function createUser(email, password) {
     try {
+      setSignupStatus('processingStatus');
       const response = await fetch(
         utilsData.apiURL + utilsData.apiSignUpRoute,
         {
@@ -24,23 +27,31 @@ export default function SignUp() {
       );
 
       if (response.status === 400) {
-        console.log('please check your credentials');
+        setSignupStatus('failedStatus');
+        setUserMessage('existingEmail');
+        statusReset();
       } else {
         const json = await response.json();
         window.localStorage.setItem('token', json.token);
-        console.log('worked');
-        console.log(json.token);
+        setSignupStatus('successStatus');
+        setUserMessage('success');
       }
     } catch (error) {
       console.log('An error occurred:', error);
     }
   }
 
+  function statusReset() {
+    setTimeout(() => {
+      setSignupStatus('initialStatus');
+    }, 1500);
+  }
+
   return (
     <div className="w-3/5 mt-32 flex justify-center items-center flex-col mx-auto">
       <h1 className="text-5xl text-sky-700 font-bold">Welcome to Dentora</h1>
       <h3 className="text-2xl mt-6">Please register a new user</h3>
-      <form action="" className="flex flex-col mt-6 w-4/6 mx-auto">
+      <form action="onSubmit" className="flex flex-col mt-6 w-4/6 mx-auto">
         <label className="flex gap-4 text-base justify-between mt-2 h-10 items-center">
           Email:
           <input
@@ -49,6 +60,7 @@ export default function SignUp() {
             className="w-3/6 pl-2 border-2  border-sky-600 rounded-lg py-1"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
+            required
           />
         </label>
 
@@ -74,25 +86,46 @@ export default function SignUp() {
             </button>
           </Link>
 
-          <button
-            id="register"
-            className="flex justify-center items-center gap-2 rounded-lg bg-gradient-to-tr from-sky-600 to-sky-900 py-2 px-10 text-center text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-            type="button"
-            data-ripple-light="true"
-            onClick={() => {
-              createUser(email, password);
-              document
-                .getElementById('register')
-                .classList.replace('flex', 'hidden');
-              document
-                .getElementById('continue')
-                .classList.replace('hidden', 'flex');
-            }}
-          >
-            <RegisterIcon /> <span>Register</span>
-          </button>
+          <div className={singupStatus === 'initialStatus' ? 'flex' : 'hidden'}>
+            <button
+              className="flex justify-center items-center gap-2 rounded-lg bg-gradient-to-tr from-sky-600 to-sky-900 py-2 px-10 text-center text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+              type="button"
+              data-ripple-light="true"
+              onSubmit={() => {
+                createUser(email, password);
+              }}
+            >
+              <RegisterIcon /> <span>Register</span>
+            </button>
+          </div>
 
-          <Link to="/CurrentSchedule" id="continue" className="hidden">
+          <div
+            className={singupStatus === 'processingStatus' ? 'flex' : 'hidden'}
+          >
+            <div
+              className="flex justify-center items-center gap-2 rounded-lg bg-gradient-to-tr from-sky-600 to-sky-900 py-2 px-10 text-center text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+              type="button"
+              data-ripple-light="true"
+            >
+              <ContinueIcon />
+              <span>Loading</span>
+            </div>
+          </div>
+
+          <div
+            to="/CurrentSchedule"
+            className={singupStatus === 'failedStatus' ? 'flex' : 'hidden'}
+          >
+            <div className="flex justify-center items-center gap-2 rounded-lg bg-gradient-to-tr from-sky-600 to-sky-900 py-2 px-10 text-center text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
+              <ContinueIcon />
+              <span>Failed</span>
+            </div>
+          </div>
+
+          <Link
+            to="/CurrentSchedule"
+            className={singupStatus === 'successStatus' ? 'flex' : 'hidden'}
+          >
             <button
               className="flex justify-center items-center gap-2 rounded-lg bg-gradient-to-tr from-sky-600 to-sky-900 py-2 px-10 text-center text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
               type="button"
@@ -104,6 +137,15 @@ export default function SignUp() {
           </Link>
         </div>
       </form>
+      <div className={userMessage === 'error' ? 'flex' : 'hidden'}>
+        <p className="mt-4">Hubo un problema, intente mas tarde.</p>
+      </div>
+      <div className={userMessage === 'existingEmail' ? 'flex' : 'hidden'}>
+        <p className="mt-4">Email already in file, please go back to login</p>
+      </div>
+      <div className={userMessage === 'success' ? 'flex' : 'hidden'}>
+        <p className="mt-4">User has been register, you can Continue</p>
+      </div>
     </div>
   );
 }
