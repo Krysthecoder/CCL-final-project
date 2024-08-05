3b4a3f14b133342972d3b1f703fca6192657c7ae
The Krys <krysthopher@gmail.com>
2024-08-05 17:49:14 -0600

    removed a (Link) import that is not been used

--- src/pages/CurrentSchedule.jsx
+++ src/pages/CurrentSchedule.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
 import { CalendarIcon } from '../icons';
 import NavBar from '../components/NavBar/index';
 import { utilsData } from '../utils/utilsData';
