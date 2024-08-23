05f6983f83570047cb78c94db2f0be416b7be5f7
krysthecoder <krysthopher@gmail.comm>
2024-08-23 02:03:38 -0600

    refactor:(current appointment) added edit icon, enhanced edit delete layout

--- src/components/AppointmentsTable/Index.jsx
+++ src/components/AppointmentsTable/Index.jsx
@@ -100,19 +100,27 @@ export default function AppointmentsTable({ rowData }) {
                 </StyledTableCell>
 
                 <StyledTableCell align="center">
-                  {/* <button>
-                    <EditIcon />
-                  </button> */}
-                  <ButtonWithIcon
-                    key={i}
-                    btnCaption=""
-                    btnClassName="hover:scale-110 ease-in-out"
-                    // btnClassName="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-                    IconComp={<TrashIcon />}
-                    onClickFn={() => {
-                      console.log(row.id);
-                    }}
-                  />
+                  <div className="flex flex-row  justify-around">
+                    <ButtonWithIcon
+                      key={i}
+                      btnCaption=""
+                      btnClassName="hover:scale-110 ease-in-out"
+                      IconComp={<EditIcon />}
+                      onClickFn={() => {
+                        console.log(row.id);
+                      }}
+                    />
+                    <ButtonWithIcon
+                      key={i}
+                      btnCaption=""
+                      btnClassName="hover:scale-110 ease-in-out"
+                      // btnClassName="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                      IconComp={<TrashIcon />}
+                      onClickFn={() => {
+                        console.log(row.id);
+                      }}
+                    />
+                  </div>
                 </StyledTableCell>
               </StyledTableRow>
             ))}
