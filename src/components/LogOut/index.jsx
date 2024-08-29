556555573048078a3c714f09ee584c94e64aa955
krysthecoder <krysthopher@gmail.comm>
2024-08-29 01:39:19 -0600

    refactor: ( LogoutBtn ) enhanced code by inplementing UseNavigate and useCallBack

--- src/components/LogOut/index.jsx
+++ src/components/LogOut/index.jsx
@@ -1,19 +1,21 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useCallback } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { LogoutIcon } from '../../icons';
 
 export default function LogoutBtn() {
-  const removeFetchedToken = () => {
+  const navigate = useNavigate();
+
+  const removeFetchedToken = useCallback(() => {
     localStorage.removeItem('fetchedToken');
     localStorage.removeItem('userId');
     localStorage.removeItem('userName');
-  };
+    navigate('/');
+  }, [navigate]);
 
   return (
-    <Link to={'/'} className="text-sm">
-      <button onClick={removeFetchedToken} className="w-28 px-4">
-        <LogoutIcon title="Logout" />
-      </button>
-    </Link>
+    <button onClick={removeFetchedToken} className="w-28 px-4">
+      <LogoutIcon title="Logout" />
+      <span className="sr-only">Logout</span>
+    </button>
   );
 }
