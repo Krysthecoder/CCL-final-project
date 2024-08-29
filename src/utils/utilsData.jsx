12b403c70da14319a9c3079ffcc957033c8058ca
krysthecoder <krysthopher@gmail.comm>
2024-08-29 01:51:30 -0600

    refactor: ( UtilsData ) removed unused data

--- src/utils/utilsData.jsx
+++ src/utils/utilsData.jsx
@@ -6,23 +6,3 @@ export const utilsData = {
   apiCreatNewAppointment: 'appointment',
   apiDeleteAppointment: 'appointment'
 };
-
-export const officeHours = [
-  { value: '09:00:00', label: '9:00 am' },
-  { value: '09:30:00', label: '9:30 am' },
-  { value: '10:00:00', label: '10:00 am' },
-  { value: '10:30:00', label: '10:30 am' },
-  { value: '11:00:00', label: '11:00 am' },
-  { value: '11:30:00', label: '11:30 am' },
-  { value: '12:00:00', label: '12:00 pm' },
-  { value: '12:30:00', label: '12:30 pm' },
-  { value: '13:00:00', label: '1:00 pm' },
-  { value: '13:30:00', label: '1:30 pm' },
-  { value: '14:00:00', label: '2:00 pm' },
-  { value: '14:30:00', label: '2:30 pm' },
-  { value: '15:00:00', label: '3:00 pm' },
-  { value: '15:30:00', label: '3:30 pm' },
-  { value: '16:00:00', label: '4:00 pm' },
-  { value: '16:30:00', label: '4:30 pm' },
-  { value: '17:00:00', label: '5:00 pm' }
-];
