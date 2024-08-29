45419bf612bf0d8323f03269cb9299b0274d34a5
krysthecoder <krysthopher@gmail.comm>
2024-08-29 01:09:57 -0600

    refactor: ( CustomSelect ) removed commented code

--- src/components/CustomSelect/index.jsx
+++ src/components/CustomSelect/index.jsx
@@ -39,26 +39,5 @@ export const CustomSelect = ({ name, label, ...props }) => {
         />
       </DemoItem>
     </LocalizationProvider>
-    // <>
-    //   <div className="flex justify-between">
-    //     <label>{label}</label>
-    //     <div className="flex flex-col w-4/6">
-    //       <select
-    //         {...field}
-    //         {...props}
-    //         className={
-    //           meta.touched && meta.error
-    //             ? 'text-sm  pl-2 border-2 w-full  border-red-600 rounded-lg py-1'
-    //             : 'text-sm  pl-2 border-2 w-full  border-sky-600 rounded-lg py-1'
-    //         }
-    //       />
-    //       {meta.touched && meta.error && (
-    //         <p className="flex justify-end text-sm mr-2 text-red-700 ">
-    //           {meta.error}
-    //         </p>
-    //       )}
-    //     </div>
-    //   </div>
-    // </>
   );
 };
