3837b6ba034ee6675097f581b838e830a9e554d0
krysthecoder <krysthopher@gmail.comm>
2024-09-02 19:11:38 -0600

    refactor: ( CustomBtn ) removed unneeded code

--- src/components/CustomBtns/index.jsx
+++ src/components/CustomBtns/index.jsx
@@ -1,17 +1,6 @@
 import React from 'react';
 import { ExclamationIcon } from '../../icons';
 
-export const CustomBtn = ({
-  className = 'custom-btn-styles',
-  text = 'This need a caption'
-}) => {
-  return (
-    <div className={className}>
-      <span>{text}</span>
-    </div>
-  );
-};
-
 export const CustomBtnInnerContent = ({
   text = 'This need a caption',
   icon = ExclamationIcon
